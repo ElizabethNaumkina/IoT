000ae35e15f72d491d67192dd009c5f3366b1027
ElizabethNaumkina <53448486+ElizabethNaumkina@users.noreply.github.com>
2019-11-18 17:45:54 +0500

    лаба 23.09

--- main.c
+++ main.c
@@ -1,48 +1,52 @@
-// Подключение библиотек
+/*
+ * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       example application for setting a periodic wakeup
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
 #include "xtimer.h"
 #include "timex.h"
 #include "periph/gpio.h"
-int state=1;
-// Обработчик прерывания по нажатию кнопки
-void btn_handler(void *arg)
+
+/* set interval to 1 second */
+#define INTERVAL (1U * US_PER_SEC)
+/*void btn_handler(void *arg)
 {
   // Прием аргументов, передаваемых из главного потока.
   (void)arg;
   // Переключение состояния пина PC8
-state=0;
-gpio_toggle(GPIO_PIN(PORT_C, 8));
-xtimer_usleep(5000000);
-gpio_toggle(GPIO_PIN(PORT_C, 8));
-state=1;
+  gpio_toggle(GPIO_PIN(PORT_C, 8));
 }
-
+*/
 
 int main(void)
 {
-  // Инициализация пина PA0, к которому подключена кнопка, как источника прерывания.
-  // GPIO_RISING - прерывание срабатывает по фронту
-  // btn_handler - имя функции обработчика прерывания
-  // NULL - ничего не передаем в аргументах
-  gpio_init_int(GPIO_PIN(PORT_A, 0), GPIO_IN, GPIO_RISING, btn_handler, NULL);
-  // Инициализация пина PC8 на выход
-	gpio_init(GPIO_PIN(PORT_C, 8), GPIO_OUT);
-	gpio_init(GPIO_PIN(PORT_C, 9), GPIO_OUT);
+	gpio_init(GPIO_PIN(PORT_C,8),GPIO_OUT);
+    xtimer_ticks32_t last_wakeup = xtimer_now();
+	gpio_init(GPIO_PIN(PORT_A, 0),GPIO_IN);
+	int state_1=0;
+    while(state_1==0)
+	{
+        xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
+        printf("slept until %" PRIu32 "\n", xtimer_usec_from_ticks(xtimer_now()));
+		state_1=gpio_read(GPIO_PIN(PORT_A, 0));
+		gpio_toggle(GPIO_PIN(PORT_C,8));
+    }
 
-
-  while(state==1){
-xtimer_usleep(5000000);
-gpio_toggle(GPIO_PIN(PORT_C,9));
+    return 0;
 }
-  return 0;
-}
-
-/*
-Задание 1. Подавите дребезг контакта кнопки. Для этого запомните время, когда вызвалось прерывание, 
-            и при последующем вызове, если не прошло 100 мс, не переключайте светодиод.
-Задание 2. Сделайте так, чтобы при удержании кнопки светодиод светился, а при отпускании - гас. 
-            В этом случае нужно заставить прерывание срабатывать и на фронт, и на спад. 
-            Это задается при инициализации прерывания макросом GPIO_BOTH. 
-            Обработчик прерывания должен узнать, его вызвал фронт или спад.
-            Для этого можно прочитать значение пина с кнопкой функцией gpio_read. 
-            Если прочитали высокое состояние - то это был фронт, если низкое - это был спад.   
-*/
